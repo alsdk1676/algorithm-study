058b599f248df6f842138bdcb7419fe0529c3c8a
alsdk1676 <alsdk6761@gmail.com>
2025-10-08 23:55:24 +0900

    Create 2751_수정렬하기2.java

---
+++ baekjoon/week04/2751_수정렬하기2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();         
+        int[] arr = new int[N];       
+
+        for(int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        for(int i = 0; i < N - 1 - i; i++) {
+            for(int j = 0; j < N - 1; j++) {
+                if(arr[j] > arr[j + 1]) {
+                    int arr2 = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = arr2;
+                }
+            }
+        }
+        
+        for(int i = 0; i < N; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
